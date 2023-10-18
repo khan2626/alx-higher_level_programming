ee6180c2cbcbecf315bb20d3ddc8da52b516e6a4
khan2626 <israelkhan217@gmail.com>
2023-10-18 19:38:54 +0100

    Task_0-6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- Creates database hbtn_0d_usa and the table states.
 CREATE DATABASE IF NOT EXIST hbtn_0d_usa;
 CREATE TABLE IF NOT EXIST states (
-       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-       name VARCHAR(256) NOT NULL);
+id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(256) NOT NULL);
