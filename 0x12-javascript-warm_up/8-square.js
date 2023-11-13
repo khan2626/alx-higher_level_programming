690e91872bdba2c1be1b46c22c911fdc533eaffb
khan2626 <israelkhan217@gmail.com>
2023-11-13 22:05:45 +0100

    Javascript-warm_up

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+    console.log("Missing size");
+} else {
+    const x = Number(process.argv[2]);
+    for (let i = 0; i < x; i++) {
+	console.log("x".repeat(x));
+    }
+}
